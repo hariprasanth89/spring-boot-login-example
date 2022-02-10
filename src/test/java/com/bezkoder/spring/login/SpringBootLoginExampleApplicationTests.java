d04acab4dcebb145d405f61b621f25aa2e075358
Hariprasanth <hariprasanth89@gmail.com>
2022-02-11 02:22:14 +0530

    Create SpringBootLoginExampleApplicationTests.java

---
+++ src/test/java/com/bezkoder/spring/login/SpringBootLoginExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezkoder.spring.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {com.bezkoder.spring.login.SpringBootLoginExampleApplicationTests.class})
+class SpringBootLoginExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
